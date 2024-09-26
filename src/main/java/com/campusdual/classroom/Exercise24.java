fcf7582a62b377537f8803753c629e523b3739a9
Sara <sarareyvalino23@gmail.com>
2024-09-26 14:56:54 +0200

    Ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -4,14 +4,23 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Exercise24 {
-    public static Queue<String> createQueue() {
 
+    public static Queue<String> createQueue() {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Smith");
+        queue.add("Montessori");
+        queue.add("Peralta");
+        queue.add("House");
+        return queue;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
+        createQueue();
 
     }
 
